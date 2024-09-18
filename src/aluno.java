74fcdf5223366cf2f628a8570a651d0e2b2d499b
Nina-AS <ninamorenapreis@gmailcom>
2024-09-18 15:43:17 -0300

    feat: class

---
+++ src/aluno.java
@@ -0,0 +1,13 @@
+public class aluno {
+        public static void main(String[] args){
+
+            int n1 = 5;
+            int n2 = 0;
+            int n3 = 10;
+            int n4 = 6;
+
+            double 
+
+
+        }
+}
