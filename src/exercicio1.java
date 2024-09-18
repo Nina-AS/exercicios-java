13cdd14949985661d72eef5d37e95ebc1f19344d
Nina-AS <ninamorenapreis@gmailcom>
2024-09-18 15:32:14 -0300

    feat: funcionario

---
+++ src/exercicio1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) throws Exception {
+       
+       Scanner ler = new Scanner(System.in);
+       int funcionario1;
+    
+       funcionario1 = ler.nextInt();
+
+       if (funcionario1 >=10 ) {
+        System.out.println("Insatisfeito");
+       }else{
+
+       }
+
+       if (funcionario1 >=40) {
+        System.out.println("Regular");
+       }else{
+
+       }
+       if (funcionario1 >=100) {
+        System.out.println("Òtimo");
+       }else{
+        
+       }
+       if (funcionario1 >=50) {
+        System.out.println("Bom");
+       }else{
+      }  
+}
+
+}
